4ac9f92d3fc0054fc3117d081979a27f0e834fd2
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-10-26 20:44:47 -0700

    Solve exercise 08 and 09
    Solved and passed all test cases of exercise 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, x) => a + x, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((a, x) => a * x, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let fac = 1;
+    if (a == 0 || a == 1) {
+        return fac;
+    } else {
+        while (a >= 1) {
+            fac *= a;
+            a --;
+        }
+        return fac;
+    }
 };
+
+//npm test calculator.spec.js
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var newstr = str.replace(/[\.,?!]/g, '').replace(/ /g, '').toLowerCase();
+    console.log(newstr);
 
+    for (i = 0; i < newstr.length; i ++) {
+        console.log(newstr[i], newstr[newstr.length - 1 - i]);
+        if (newstr[i] != newstr[newstr.length - 1 - i]) return false;
+    }
+
+    return true;
 };
+
+//npm test palindromes.spec.js
 
 // Do not edit below this line
 module.exports = palindromes;
